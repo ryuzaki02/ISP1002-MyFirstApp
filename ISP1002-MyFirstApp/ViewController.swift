79538ee76d47ad84485b79d5c59eff2be5a6fded
Aman <thakur.aman77@yahoo.com>
2022-07-07 15:03:41 -0400

    First Commit - Created an empty Xcode project

---
+++ ISP1002-MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISP1002-MyFirstApp
+//
+//  Created by Aman on 07/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
